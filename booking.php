7ffa7ec13835c1d756f4e2dd24d2bfba1bc26091
excellentdesign99 <excellentdesigncommunications@gmail.com>
2023-06-28 23:58:14 +0530

    correction in previous changes

--- booking.php
+++ booking.php
@@ -6,12 +6,12 @@
         <div class="container-fluid page-header mb-5 p-0" style="background-image: url(img/carousel-1.jpg);">
             <div class="container-fluid page-header-inner py-5">
                 <div class="container text-center pb-5">
-                    <h1 class="display-3 text-white mb-3 animated slideInDown">Booking</h1>
+                    <h1 class="display-3 text-white mb-3 animated slideInDown">Plan your travel with us</h1>
                     <nav aria-label="breadcrumb">
                         <ol class="breadcrumb justify-content-center text-uppercase">
                             <li class="breadcrumb-item"><a href="#">Home</a></li>
                             <li class="breadcrumb-item"><a href="#">Pages</a></li>
-                            <li class="breadcrumb-item text-white active" aria-current="page">Plan your travel with us</li>
+                            <li class="breadcrumb-item text-white active" aria-current="page">Booking</li>
                         </ol>
                     </nav>
                 </div>
