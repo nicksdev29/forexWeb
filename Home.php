6bf7d8e6563d4a8e73cce6c2179afdf7353c03c5
excellentdesign99 <excellentdesigncommunications@gmail.com>
2023-06-29 15:52:09 +0530

    changes icon

--- Home.php
+++ Home.php
@@ -397,7 +397,8 @@ Europe and African Safari etc.</p>
                         <a class="service-item rounded" href="">
                             <div class="service-icon bg-transparent border rounded p-1">
                                 <div class="w-100 h-100 border rounded d-flex align-items-center justify-content-center">
-                                    <i class="fa fa-utensils fa-2x text-primary"></i>
+                                <i class="fa fa-usd" aria-hidden="true"></i>
+
                                 </div>
                             </div>
                             <h5 class="mb-3">Flight (Domestic and International)</h5>
@@ -452,6 +453,7 @@ Europe and African Safari etc.</p>
             </div>
         </div>
         <!-- Service End -->
+
 
 
         <!-- Testimonial Start -->
