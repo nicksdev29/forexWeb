9c6d1d8d9f9d093e2e8273b9da6a9ce1c976dfd1
excellentdesign99 <excellentdesigncommunications@gmail.com>
2023-07-02 16:57:02 +0530

    .

--- career.php
+++ career.php
@@ -6,7 +6,7 @@
         <div class="container-fluid page-header mb-5 p-0" style="background-image: url(img/carousel-1.jpg);">
             <div class="container-fluid page-header-inner py-5">
                 <div class="container text-center pb-5">
-                    <h1 class="display-3 text-white mb-3 animated slideInDown">career in samfxt</h1>
+                    <h1 class="display-3 text-white mb-3 animated slideInDown">Career in samfxt</h1>
                     <nav aria-label="breadcrumb">
                         <ol class="breadcrumb justify-content-center text-uppercase">
                             <li class="breadcrumb-item"><a href="Home.php">Home</a></li>
