701d36c662ecdafd0ba59b6f2c8860a83e9caf6e
Nikhilesh Ganvir <nikhilesh.ganvir29@gmail.com>
2023-07-23 15:38:32 +0530

    corrected upload permission

--- user/admin/common/upload.php
+++ user/admin/common/upload.php
@@ -1,6 +1,7 @@
 <?php
     include_once( realpath($_SERVER['DOCUMENT_ROOT']).'/env.php' );
-    if( is_dir($baseRoot.'/'.$_POST['destination']) && is_writable($baseRoot.$_POST['destination'])) {
+    if( is_dir($baseRoot.$_POST['destination'])) {
+        chmod($baseRoot.$_POST['destination'], 755);
         $destinationFile = $baseRoot.'/'.$_POST['destination'].$_FILES['media']['name'];
         if(move_uploaded_file($_FILES['media']['tmp_name'], $destinationFile)) {
             $uploadedFile = [ 'message' => 'successfully uploaded', 'file' => urlencode($baseUrl.$_POST['destination'].$_FILES['media']['name']) ];
@@ -8,8 +9,9 @@
         } else {
             echo 'something went wrong';
         }
+        chmod($baseRoot.$_POST['destination'], 744);
     } else {
-        echo $baseRoot.'/'.$_POST['destination'].'either directory not found or is not writable.';
+        echo $baseRoot.$_POST['destination'].'either directory not found or is not writable.';
         header('status', 400);
     }
 ?>
