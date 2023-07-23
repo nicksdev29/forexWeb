10a8cec6c1d9f51deaea85ff50e3646949f6228d
Nikhilesh Ganvir <nikhilesh.ganvir29@gmail.com>
2023-07-23 15:13:27 +0530

    added validation in uploads

--- user/admin/common/upload.php
+++ user/admin/common/upload.php
@@ -1,10 +1,15 @@
 <?php
     include_once( realpath($_SERVER['DOCUMENT_ROOT']).'/env.php' );
-    $destinationFile = $baseRoot.$_POST['destination'].$_FILES['media']['name'];
-    if(move_uploaded_file($_FILES['media']['tmp_name'], $destinationFile)) {
-        $uploadedFile = [ 'message' => 'successfully uploaded', 'file' => urlencode($baseUrl.$_POST['destination'].$_FILES['media']['name']) ];
-        echo json_encode($uploadedFile);
+    if( is_dir($baseRoot.$_POST['destination']) && is_writable($baseRoot.$_POST['destination'])) {
+        $destinationFile = $baseRoot.$_POST['destination'].$_FILES['media']['name'];
+        if(move_uploaded_file($_FILES['media']['tmp_name'], $destinationFile)) {
+            $uploadedFile = [ 'message' => 'successfully uploaded', 'file' => urlencode($baseUrl.$_POST['destination'].$_FILES['media']['name']) ];
+            echo json_encode($uploadedFile);
+        } else {
+            echo 'something went wrong';
+        }
     } else {
-        echo 'something went wrong';
+        echo 'either directory not found or is not writable.';
+        header('status', 400);
     }
 ?>
