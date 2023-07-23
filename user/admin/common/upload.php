4b10d5ee0131d5ea7e8ed4c028d90c1e84fbb929
Nikhilesh Ganvir <nikhilesh.ganvir29@gmail.com>
2023-07-23 14:58:26 +0530

    corrected include in upload for linux

--- user/admin/common/upload.php
+++ user/admin/common/upload.php
@@ -1,7 +1,6 @@
 <?php
-    include_once($_SERVER['DOCUMENT_ROOT'].'\env.php');
+    include_once( realpath($_SERVER['DOCUMENT_ROOT']).'/env.php' );
     $destinationFile = $baseRoot.$_POST['destination'].$_FILES['media']['name'];
-    print_r($_FILES['media']);
     if(move_uploaded_file($_FILES['media']['tmp_name'], $destinationFile)) {
         $uploadedFile = [ 'message' => 'successfully uploaded', 'file' => urlencode($baseUrl.$_POST['destination'].$_FILES['media']['name']) ];
         echo json_encode($uploadedFile);
