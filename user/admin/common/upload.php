0d6d18b4492d1a0894f567936bf4d625484f8f66
Nikhilesh Ganvir <nikhilesh.ganvir29@gmail.com>
2023-07-23 16:02:27 +0530

    reverted permission code block

--- user/admin/common/upload.php
+++ user/admin/common/upload.php
@@ -1,7 +1,6 @@
 <?php
     include_once( realpath($_SERVER['DOCUMENT_ROOT']).'/env.php' );
     if( is_dir($baseRoot.$_POST['destination'])) {
-        chmod($baseRoot.$_POST['destination'], 777);
         $destinationFile = $baseRoot.'/'.$_POST['destination'].$_FILES['media']['name'];
         if(move_uploaded_file($_FILES['media']['tmp_name'], $destinationFile)) {
             $uploadedFile = [ 'message' => 'successfully uploaded', 'file' => urlencode($baseUrl.$_POST['destination'].$_FILES['media']['name']) ];
@@ -9,7 +8,6 @@
         } else {
             echo 'something went wrong';
         }
-        chmod($baseRoot.$_POST['destination'], 755);
     } else {
         echo $baseRoot.$_POST['destination'].'either directory not found or is not writable.';
         header('status', 400);
