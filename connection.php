3094d10facaa5b021c88df93bde0b2c68f8c23af
excellentdesign99 <excellentdesigncommunications@gmail.com>
2023-10-04 23:55:56 +0530

    undo connection

--- connection.php
+++ connection.php
@@ -2,10 +2,10 @@
 $host="localhost:3306";
 $user="root";
  $pass="c+Hc9_G%=sGyY)xy";
- // $db="samfxt_db";
+ $db="samfxt_db";
 
 // $pass="";
-$db="samfxt";
+// $db="samfxt";
 $con=mysqli_connect($host,$user,$pass,$db);
 
 
