6d5a645faa1c9564b1600bfe075263263cad8608
excellentdesign99 <135546683+excellentdesign99@users.noreply.github.com>
2023-12-11 04:52:53 -0800

    styling in our team

--- Home.php
+++ Home.php
@@ -569,7 +569,7 @@ Europe and African Safari etc.</p>
                             </div>
                             <div class="text-center p-4 mt-3">
                                 <h5 class="fw-bold mb-0">Mr. Supratim Roy</h5>
-                                <small>Director cum Fxchange<br>Supratim Roy has more than 28 years of experience in Foreign Exchange. He started his career in Thomas Cook India and later he joined American Experience . He also worked in Hong Kong , Singapore and Australia for 17 years.</small>
+                                Director cum Fxchange<br><small>Supratim Roy has more than 28 years of experience in Foreign Exchange. He started his career in Thomas Cook India and later he joined American Experience . He also worked in Hong Kong , Singapore and Australia for 17 years.</small>
                             </div>
                         </div>
                     </div>
@@ -585,7 +585,7 @@ Europe and African Safari etc.</p>
                             </div>
                             <div class="text-center p-4 mt-3">
                                 <h5 class="fw-bold mb-0">Mr. Mithu Roy</h5>
-                                <small>Director<br>Mithu Roy has experienced in Travel and Tourism Management. She is also involved in Equity Trading. At SAM FXT she is looking after the travel part.</small>
+                                Director<br><small>Mithu Roy has experienced in Travel and Tourism Management. She is also involved in Equity Trading. At SAM FXT she is looking after the travel part.</small>
                             </div>
                         </div>
                     </div>
@@ -601,7 +601,7 @@ Europe and African Safari etc.</p>
                             </div>
                             <div class="text-center p-4 mt-3">
                                 <h5 class="fw-bold mb-0">Mr. Neeraj Dhopte</h5>
-                                <small>IT Advisor<br>M.Tech-– Electrical Engineering
+                                IT Advisor<br><small>M.Tech-– Electrical Engineering
 Indian Institute of Technology, Roorkee ( IIT Roorkee) Co-Founder & CTO Translab Technologies, Bangalore
 </small>
                             </div>
