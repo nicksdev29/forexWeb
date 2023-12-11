f654a6ceff8a0c7bf2d13b30549f3f1472c87e7e
excellentdesign99 <135546683+excellentdesign99@users.noreply.github.com>
2023-12-11 04:42:27 -0800

    Our team updated

--- Home.php
+++ Home.php
@@ -568,7 +568,7 @@ Europe and African Safari etc.</p>
                                 </div>
                             </div>
                             <div class="text-center p-4 mt-3">
-                                <h5 class="fw-bold mb-0">Mr.Supratim Roy</h5>
+                                <h5 class="fw-bold mb-0">Mr. Supratim Roy</h5>
                                 <small>Director cum Fxchange</small>
                             </div>
                         </div>
@@ -584,7 +584,7 @@ Europe and African Safari etc.</p>
                                 </div>
                             </div>
                             <div class="text-center p-4 mt-3">
-                                <h5 class="fw-bold mb-0">Mithu Roy</h5>
+                                <h5 class="fw-bold mb-0">Mr. Mithu Roy</h5>
                                 <small>Director</small>
                             </div>
                         </div>
@@ -600,7 +600,7 @@ Europe and African Safari etc.</p>
                                 </div>
                             </div>
                             <div class="text-center p-4 mt-3">
-                                <h5 class="fw-bold mb-0">Neeraj Dhopte</h5>
+                                <h5 class="fw-bold mb-0">Mr. Neeraj Dhopte</h5>
                                 <small>IT Advisor</small>
                             </div>
                         </div>
