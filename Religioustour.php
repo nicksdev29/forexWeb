3ae17f59c61b589679dec6f4323dc1ab92bc8640
excellentdesign99 <135546683+excellentdesign99@users.noreply.github.com>
2023-12-11 04:25:21 -0800

    same changes in religious tour page

--- Religioustour.php
+++ Religioustour.php
@@ -6,7 +6,7 @@
         <div class="container-fluid page-header mb-5 p-0" style="background-image: url(img/carousel-1.jpg);">
             <div class="container-fluid page-header-inner py-5">
                 <div class="container text-center pb-5">
-                    <h1 class="display-3 text-white mb-3 animated slideInDown">DOMESTIC AND INTERNATIONAL TOUR</h1>
+                    <h2 class="display-3 text-white mb-3 animated slideInDown">DOMESTIC AND INTERNATIONAL TOUR</h2>
                     <nav aria-label="breadcrumb">
                         <ol class="breadcrumb justify-content-center text-uppercase">
                             <li class="text-white">We will book your flight tickets for your Travel anywhere both ... Domestic and International</li>
