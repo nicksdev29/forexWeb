d0fb13274321b8bbd391edb72daa46fd771ae293
Nikhilesh Ganvir <nikhilesh.ganvir29@gmail.com>
2024-02-17 01:37:40 +0530

    reverted connection and cofiguration

--- connection.php
+++ connection.php
@@ -1,11 +1,11 @@
 <?php
 $host="localhost:3306";
 $user="root";
-// $pass="c+Hc9_G%=sGyY)xy";
-// $db="samfxt_db";
+$pass="c+Hc9_G%=sGyY)xy";
+$db="samfxt_db";
 
-$pass="";
-$db="samfxt";
+// $pass="";
+// $db="samfxt";
 $con=mysqli_connect($host,$user,$pass,$db);
 
 
