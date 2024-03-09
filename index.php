3d086f426f823927137c178c7a6aa9b8bc8e4787
excellentdesign99 <135546683+excellentdesign99@users.noreply.github.com>
2024-03-09 14:45:20 +0530

    .

--- index.php
+++ index.php
@@ -212,7 +212,7 @@ Europe and African Safari etc.</p>
                                 <!-- <small>Profession</small> -->
                             </div>
                         </div>
-                        <i class="fa fa-quote-right fa-3x text-primary position-absolute end-0 bottom-0 me-4 mb-n1"></i>
+                        <!-- <i class="fa fa-quote-right fa-3x text-primary position-absolute end-0 bottom-0 me-4 mb-n1"></i> -->
                     </div>
 
                     <?php
